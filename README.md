8cd1ab7f97088a97ea4db0b88da216763e6fa520
Cloud User <ec2-user@ip-172-31-24-147.ec2.internal>
2017-07-09 16:39:49 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# httpdmanish
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: httpdmanish
+# Recipe:: default
+#
+# Copyright:: 2017, The Authors, All Rights Reserved.
